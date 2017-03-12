11e6981bc65823a8fbe9c5524dd90aa8f325cfea
陈超 <angelgreen.chao@gmail.com>
2017-03-12 12:36:04 +0800

    fix:delete js html css files

--- src/main/webapp/js/firstPage.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#window').load('/html/window.html',function() {
-		var win_ui = new win();
-		win_ui.init();
-		$('.btn_next').click(function() {
-			win_ui.showPop();
-		});
-	});
-});
